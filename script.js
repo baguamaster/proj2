09effd5c27b35cd42ea1f0a48bd08f49d3ff3fc1
Kevin Ho <traderkev54@yahoo.com>
2021-01-21 14:54:09 +0800

    parsing video into iframes

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 $(document).ready(function() {
-    var API_KEY = "AIzaSyCJRYzVLGW2_Ex3A9r3ZsyUFq0rVNMYL3I"
-    $("form").submit(function(event) {
-        event.preventDefault()
+            var API_KEY = "AIzaSyCJRYzVLGW2_Ex3A9r3ZsyUFq0rVNMYL3I"
 
-        var search = $("#search").val()
+            var video = ''
 
-        videosearch(API_KEY, search, 21)
+            var videos = $("#videos")
 
-    })
 
-    function videosearch(key, search, maxResults) {
+            $("#form").submit(function(event) {
+                event.preventDefault()
+                alert("form submitted")
 
-        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function(data) console.log(data))
-    }
-})
+                var search = $("#search").val()
+
+                videosearch(API_KEY, search, 21)
+
+            })
+
+            function videosearch(key, search, maxResults) {
+
+                $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function(data) {
+                        console.log(data)
+
+                        data.items.forEach(item => {
+                            video = ` <iframe width="420" height="345" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                            </iframe>
+                            `
+                        })
+                    }
+                })
